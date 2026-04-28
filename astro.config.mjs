2de4dc31d4a1613491547a4d62bc0973ddec8011
Mucunguzi Moses <moemucu@gmail.com>
2026-04-28 03:50:48 +0300

    Revert to previous design (cleaner layout)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://katolabanfoundation.org',
   base: '/',
-  integrations: [tailwind()],
 });
