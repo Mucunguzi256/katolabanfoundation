2515f845f1aeae68f66d6bded150cabf76175534
Mucunguzi Moses <moemucu@gmail.com>
2026-04-30 07:12:06 +0300

    feat: add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://katolabanfoundation.org',
-  base: '/',
+  site: 'https://mucunguzi256.github.io',
+  base: '/katolabanfoundation',
   integrations: [tailwind()],
+  build: {
+    format: 'file'
+  }
 });
